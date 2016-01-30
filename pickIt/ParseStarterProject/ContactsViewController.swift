bc92d279cf0d0e7ece3e5df0c19e5494a3af4110
Brian Wichers <brianwichers@gmail.com>
2016-01-30 15:17:56 -0800

    contacts code and test image picker code readied

--- pickIt/ParseStarterProject/ContactsViewController.swift
+++ pickIt/ParseStarterProject/ContactsViewController.swift
@@ -11,49 +11,64 @@ import Contacts
 import Parse
 
 
-class ContactsViewController: UIViewController {
-
+class ContactsViewController: UITableViewController  {
+    
+    var myContacts : [CNContact] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let toFetch = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey]
+        let toFetch = [CNContactFormatter.descriptorForRequiredKeysForStyle(.FullName),
+            CNContactEmailAddressesKey,
+            CNContactPhoneNumbersKey,
+            CNContactImageDataAvailableKey,
+            CNContactThumbnailImageDataKey]
         
         let request = CNContactFetchRequest(keysToFetch: toFetch)
-
+        
         let store = CNContactStore()
         
         do{
             try store.enumerateContactsWithFetchRequest(request) {
                 contact, stop in
-                print(contact.givenName)
-                print(contact.familyName)
-                print(contact.identifier)
+                
+                let theContact = contact as CNContact
+                self.myContacts.append(theContact)
+                
             }
         } catch let err{
             print(err)
         }
-        
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myContacts.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("contact_cell", forIndexPath: indexPath)
+        cell.textLabel?.text = CNContactFormatter.stringFromContact(myContacts[indexPath.item], style: .FullName)
+        return cell
+    }
+    
+    @IBAction func contact_button(sender: AnyObject) {
+        
+    }
     @IBOutlet weak var contactsTable: UITableView!
     
+    //override func viewDidAppear(animated: Bool) {
+    //    super.viewDidAppear(animated)
+    //    contactsTable.reloadData()
+    //}
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
-
---
+++ pickIt/ParseStarterProject/testViewController.swift
@@ -0,0 +1,73 @@
+//
+//  testViewController.swift
+//  pickIt
+//
+//  Created by Brian Wichers on 1/30/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class testViewController: UIViewController {
+    
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var image2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        image1.image = UIImage(named: "example_image.png")
+        image2.image = UIImage(named: "example_image.png")
+        let tapGestureRecognizer1 = UITapGestureRecognizer(target:self, action:Selector("image1Tapped:"))
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target:self, action:Selector("image2Tapped:"))
+        image1.userInteractionEnabled = true
+        image1.addGestureRecognizer(tapGestureRecognizer1)
+        image2.userInteractionEnabled = true
+        image2.addGestureRecognizer(tapGestureRecognizer2)
+        
+    }
+    
+    func image1Tapped(img: AnyObject)
+    {
+        image1.fadeOut()
+        image2.fadeIn()
+    }
+    func image2Tapped(img: AnyObject)
+    {
+        image2.fadeOut()
+        image1.fadeIn()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+public extension UIImageView {
+    
+    /**
+     Fade in a view with a duration
+     
+     - parameter duration: custom animation duration
+     */
+    func fadeIn(duration duration: NSTimeInterval = 1.0) {
+        UIImageView.animateWithDuration(duration, animations: {
+            self.alpha = 1.0
+        })
+    }
+    
+    /**
+     Fade out a view with a duration
+     
+     - parameter duration: custom animation duration
+     */
+    func fadeOut(duration duration: NSTimeInterval = 1.0) {
+        UIImageView.animateWithDuration(duration, animations: {
+            self.alpha = 0.2
+        })
+    }
+    
+}
