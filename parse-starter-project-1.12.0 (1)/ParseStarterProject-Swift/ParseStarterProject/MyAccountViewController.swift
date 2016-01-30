d642ae4c8a033b3262518e92cfb5c2b5f576b061
Matthew Wang <majwang@ucsc.edu>
2016-01-29 20:42:10 -0800

    account stuff

--- parse-starter-project-1.12.0 (1)/ParseStarterProject-Swift/ParseStarterProject/MyAccountViewController.swift
+++ parse-starter-project-1.12.0 (1)/ParseStarterProject-Swift/ParseStarterProject/MyAccountViewController.swift
@@ -7,21 +7,80 @@
 //
 
 import UIKit
-
+import Parse
 class MyAccountViewController: UIViewController {
 
+    @IBOutlet weak var emailValue: UITextField!
+    @IBOutlet weak var nameValue: UITextField!
+    @IBOutlet weak var scoreValue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        editing = false
+        let currentUser = PFUser.currentUser()
+        if currentUser != nil {
+            let currentId = currentUser?.objectId
+            let query = PFUser.query()
+            query!.getObjectInBackgroundWithId(currentId!) {
+                (myself: PFObject?, error: NSError?) -> Void in
+                if error == nil && myself != nil {
+                    self.emailValue.text = myself!["email"]as? String
+                    self.nameValue.text = myself!["username"]as?String
+                    self.scoreValue.text = myself!["score"]as?String
+                }
+            
+            }
+        }
 
-        // Do any additional setup after loading the view.
+        // Dany additional setup after loading the view.
+    }
+    func notEditingAccount(){
+        emailValue.userInteractionEnabled = false
+        nameValue.userInteractionEnabled = false
+        scoreValue.userInteractionEnabled = false
+        editing = false
+        editButtonItem().title = "Edit Account"
+        
+        let currentId = PFUser.currentUser()?.objectId
+        let query = PFUser.query()
+        query!.getObjectInBackgroundWithId(currentId!) {
+            (myself: PFObject?, error: NSError?) -> Void in
+            if error == nil && myself != nil {
+                myself!["name"] = self.nameValue.text
+                myself!["email"] = self.emailValue.text
+                
+                myself!.saveInBackgroundWithBlock {
+                    (success, error) -> Void in
+                }
+            }
+            else {
+                print(error)
+            }
+        }
     }
 
+    func editingAccount(){
+        emailValue.userInteractionEnabled = true
+        nameValue.userInteractionEnabled = true
+        editButtonItem().title = "Save"
+        editing = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
+    @IBAction func editAccount(sender: AnyObject) {
+        if editing == false{
+            editingAccount()
+        }
+        if editing == true{
+            notEditingAccount()
+            
+        }
+    }
     /*
     // MARK: - Navigation
 
