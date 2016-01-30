cca489cbd0c14473a67d7f6a89d29858490008dc
Loren Yeung <loren.w.yeung@gmail.com>
2016-01-29 22:00:51 -0800

    news feed work

---
+++ pickIt/CardUITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardUITableViewCell.swift
+//  pickIt
+//
+//  Created by Loren Yeung on 29/4/2016.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class CardUITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
